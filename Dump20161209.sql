2834dbe4bf76e5ffbac94c29417eb23e690a4744
Noomie <naomi_taysha@hotmail.com>
2016-12-09 16:45:00 +0100

    Add files via upload

--- Dump20161209.sql
+++ Dump20161209.sql
@@ -27,7 +27,7 @@ DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `Name` varchar(45) NOT NULL,
   `General_exp_date` date DEFAULT NULL,
-  `ID` int(11) NOT NULL,
+  `ID` int(15) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -38,6 +38,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES ('Rood vlees',NULL,1),('Vleeswaren',NULL,2),('Zuivel',NULL,3),('Vis',NULL,4),('Groente',NULL,5),('Gevogelte',NULL,6),('Vega',NULL,7),('Kant-en-klaar',NULL,8),('Eieren',NULL,9),('Overige',NULL,10),('Drinken',NULL,11);
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,9 +50,9 @@ DROP TABLE IF EXISTS `fridge`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `fridge` (
-  `ID` int(11) NOT NULL,
+  `ID` int(15) NOT NULL,
   `Add_date` varchar(45) NOT NULL,
-  `Product` int(13) NOT NULL,
+  `Product` char(15) NOT NULL,
   PRIMARY KEY (`ID`,`Add_date`,`Product`),
   KEY `product_id_idx` (`Product`),
   CONSTRAINT `product_id` FOREIGN KEY (`Product`) REFERENCES `product` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -75,11 +76,11 @@ DROP TABLE IF EXISTS `product`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `product` (
-  `ID` int(13) NOT NULL,
+  `ID` char(15) NOT NULL,
   `Name` varchar(45) DEFAULT NULL,
   `Expiration_time` date DEFAULT NULL,
   `Description` varchar(45) DEFAULT NULL,
-  `Category` int(11) NOT NULL,
+  `Category` int(15) NOT NULL,
   `Date_bought` date DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `fk_Category_idx` (`Category`),
@@ -93,6 +94,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES ('051469122401','Juicy Water','2016-11-03','Water met een smaakje.',11,'2016-10-01');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -105,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-12-09 15:12:08
+-- Dump completed on 2016-12-09 16:38:26
