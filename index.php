4dea532c591015487285b4dc59078bdd2186b524
lamram <kelvin7039@gmail.com>
2016-12-09 15:13:21 +0100

    changed index to php and added inits

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once('init.php');
+?>
+<doctype html>
+
+<html>
+    <head>
+        <title>Fridge Thing</title>
+        <meta charset="windows-1252">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="favicon.ico">
+        <link rel='stylesheet' href="assets/css/style.css" type="text/css" <!--media='screen and (min-width: 0px) and (max-width: 1700px)'--> />
+    </head>
+    <body>
+        <main>
+            <p>hello</p>
+        </main>
+    </body>
+</html>
