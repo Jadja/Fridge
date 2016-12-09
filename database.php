906e7c7fabd2c785f9895c983b8c9ffe20c93327
lamram <kelvin7039@gmail.com>
2016-12-09 15:08:43 +0100

    added more gets, insert, update and delete

--- database.php
+++ database.php
@@ -30,3 +30,65 @@ function SelectAllFromTable($TableName, $WhereArray, $Options) {
 	}
 	return $Result->fetchAll(PDO::FETCH_ASSOC);
 }
+
+function SelectFirstFromTable($TableName, $WhereArray, $Options) {
+	$Rows = SelectAllFromTable($TableName, $WhereArray, $Options);
+	if (!$Rows) {
+		return false;
+	}
+	return $Rows[0];
+}
+
+function GetAllRowsInTable($TableName) {
+	global $Database;
+	$Result = $Database->query("SELECT * FROM $TableName");
+	if (!$Result || $Result->rowCount() == 0) {
+		return false;
+	}
+	return $Result->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function GetLastRowsInTable($TableName, $Limit) {
+	global $Database;
+	$Result = $Database->query("SELECT * FROM $TableName ORDER BY ID DESC LIMIT 0, $Limit");
+	if (!$Result || $Result->rowCount() == 0) {
+		return false;
+	}
+	return $Result->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function InsertRowIntoTable($TableName, $Values) {
+	global $Database;
+	if (!$Values) {
+		return;
+	}
+	$Query = "INSERT INTO $TableName(";
+	$ValuesString = '';
+	foreach ($Values as $Field => $Value) {
+		$Query .= $Field . ',';
+		$ValuesString .= $Value . ',';
+	}
+	$Query = substr($Query, 0, -1);
+	$ValuesString = substr($ValuesString, 0, -1);
+	$Query .= ") VALUES($ValuesString)";
+	$Database->query($Query);
+}
+
+function UpdateRowInTable($TableName, $Where, $SetValues) {
+	global $Database;
+	$Query = "UPDATE $TableName SET ";
+	foreach ($SetValues as $Name => $Value) {
+		$Query .= "$Name = $Value,";
+	}
+	$Query = substr($Query, 0, -1) . " $Where";
+	$Database->query($Query);
+}
+
+function DeleteRowFromTable($TableName, $Where) {
+	global $Database;
+	$Database->query("DELETE FROM $TableName $Where");
+}
+
+function SelectByID($TableName, $ID) {
+	return SelectFirstFromTable($TableName, array('ID' => intval($ID)), '');
+}
