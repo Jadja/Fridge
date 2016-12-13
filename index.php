0ca7f5b385a4caaa1edf1d62f0a3d52b1ff3a03c
Jadja Desktop <jadjadebeste@gmail.com>
2016-12-13 15:14:14 +0100

    Divs example

--- index.php
+++ index.php
@@ -18,14 +18,59 @@ require_once('init.php');
                 <ul>
                 </ul>
             </nav>
-            <p>
-            <?php
-            $test = $db->SelectAllFromTable('category', '', '');
-            for ($i = 0; $i < count($test); $i++) {
-                echo $test[$i]['Name'] . '<br />';
-            }
-            ?>
-            </p>
+			<div id="product" style="background-color:blue">
+				<h1>Category</h1>
+			</div>
+			<div id="product" style="background-color:blue">
+				<h1>Category</h1>
+			</div>
+			<div id="product" style="background-color:blue">
+				<h1>Vlees & Vis</h1>
+				<p>This category is currently selected</p>
+			</div>
+			<div id="product" style="background-color:red">
+				<h2>Kip</h2>
+				<p>Houdbaar tot: Vandaag (over 0 dagen)</p>
+			</div>
+			<div id="product" style="background-color:yellow">
+				<h2>Koe</h2>
+				<p>Houdbaar tot: 15-12-2016 (over 2 dagen)</p>
+			</div>
+			<div id="product" style="background-color:yellow">
+				<h2>Varken</h2>
+				<p>Houdbaar tot: 17-12-2016 (over 4 dagen)</p>
+			</div>
+			<div id="product" style="background-color:green">
+				<h2>Kogelvis</h2>
+				<p>Houdbaar tot: 24-12-2016 (over 11 dagen)</p>
+			</div>
+			<div id="product" style="background-color:green">
+				<p>Filler</p>
+			</div>
+			<div id="product" style="background-color:green">
+				<p>Filler</p>
+			</div>
+			<div id="product" style="background-color:green">
+				<p>Filler</p>
+			</div>
+			<div id="product" style="background-color:green">
+				<p>Filler</p>
+			</div>
+			<div id="product" style="background-color:lightgreen">
+				<p>Filler</p>
+			</div>
+			<div id="product" style="background-color:lightgreen">
+				<p>Filler</p>
+			</div>
+			<div id="product" style="background-color:blue">
+				<h1>Category</h1>
+			</div>
+			<div id="product" style="background-color:blue">
+				<h1>Category</h1>
+			</div>
+			<div id="product" style="background-color:blue">
+				<h1>Category</h1>
+			</div>
         </main>
     </body>
 </html>
